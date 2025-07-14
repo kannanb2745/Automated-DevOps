4b48ac969193f229e2dde82f3c79508bbcd788e0
KANNAN B <kannan.b2745@gmail.com>
2025-07-14 13:51:00 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return jsonify({'message': 'Hello, CI/CD World!'})
+        return jsonify({'message': 'Hello, CI/CD World works!'})
 
     return app
 
